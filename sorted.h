32ce4e4efff9127ebef211b329d39df0849ac39c
Vitor Pavani <vitorpavani@poli.ufrj.br>
2019-10-07 10:54:11 -0300

    move a estrutura do header do arquivo para o file.h

--- sorted.h
+++ sorted.h
@@ -1,23 +1,10 @@
 #ifndef SORTED
 #define SORTED
+
 #include <time.h>
+#include "data.h"
 
-#define SORTED_R_PATH "sorted.bin" 
-#define BUFFER_R_PATH "sortedbuffer.bin" 
-#define SORTED_FILE_NAME "Registros Ordenados"
-
-struct sortedHeader{
-    char fileName[256];
-    time_t creationStamp;
-    time_t modStamp;
-}SORTED_HEADER;
-
-struct bufferHeader{
-    time_t modStamp;
-    unsigned int count;
-};
-
-void createSortedFile();
+void sortedInsert(DATA *data);
 
 
 #endif
